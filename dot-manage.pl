4574dda2938c1c04f8a15d6708dc9aba9433ce52
Christopher Tombleson <chris@cribznetwork.com>
2014-02-24 16:04:48 +1300

    Updated .gitignore

--- dot-manage.pl
+++ dot-manage.pl
@@ -3,6 +3,7 @@ use strict;
 use warnings;
 use Data::Dumper;
 use File::chdir;
+use Sys::Hostname;
 
 my %commands = (
     init => \&init,
@@ -30,18 +31,18 @@ sub init {
     my $home = $ENV{"HOME"};
     my $dotDir = $home . "/.dotmanage";
 
-    unless(-d $dotDir or mkdir $dotDir) {
+    unless (-d $dotDir or mkdir $dotDir) {
         print "Unable to create directory: " . $dotDir . "\n";
     }
-
-    print "If you haven't already create your git repo on your git server or github\n";
-    print "Enter the git repo url: ";
-    my $repoUrl = <STDIN>;
-    chomp($repoUrl);
 
     if (-d $dotDir . "/repo") {
         print "Dot Manage has already been initialized!!!\n";
     } else {
+        print "If you haven't already create your git repo on your git server or github\n";
+        print "Enter the git repo url: ";
+        my $repoUrl = <STDIN>;
+        chomp($repoUrl);
+
         my $cmd = "git clone " . $repoUrl . " " . $dotDir . "/repo";
         system($cmd);
     }
@@ -53,4 +54,14 @@ sub pull {
         print "Getting latest changes from git!\n";
         system("git pull");
     }
-};
+}
+
+sub symlinkFiles {
+    my $repoDir = $ENV{"HOME"} . "/.dotmanage/repo";
+    my $hostname = hostname;
+
+    unless (-d $repoDir . "/" . $hostname or -d $repoDir . "/_default") {
+        print "There is no directory for: " . $hostname . " or a _default directory!!!\n";
+        exit;
+    }
+}
